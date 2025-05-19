dea5b2c87fc65062cd3aa3b9a3eda321aab958d7
Sancarock <sancanight@gmail.com>
2025-05-19 10:29:18 -0300

    Update ml.js

--- propaganda/js/ml.js
+++ propaganda/js/ml.js
@@ -1,13 +1,13 @@
 const banners = [
   { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/23WaKH6" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/2xeEUcx" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/2XqExAw" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/1v1fK1A" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/1cGGSex" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/2uMR4Gb" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/3277kLr" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/1Erm7HD" },
-  { img: "https://sancarock.com/propaganda/img/001.gif", link: "https://mercadolivre.com/sec/2hhCKB3" }
+  { img: "https://sancarock.com/propaganda/img/002.gif", link: "https://mercadolivre.com/sec/2xeEUcx" },
+  { img: "https://sancarock.com/propaganda/img/003.gif", link: "https://mercadolivre.com/sec/2XqExAw" },
+  { img: "https://sancarock.com/propaganda/img/004.gif", link: "https://mercadolivre.com/sec/1v1fK1A" },
+  { img: "https://sancarock.com/propaganda/img/005.gif", link: "https://mercadolivre.com/sec/1cGGSex" },
+  { img: "https://sancarock.com/propaganda/img/006.gif", link: "https://mercadolivre.com/sec/2uMR4Gb" },
+  { img: "https://sancarock.com/propaganda/img/007.gif", link: "https://mercadolivre.com/sec/3277kLr" },
+  { img: "https://sancarock.com/propaganda/img/008.gif", link: "https://mercadolivre.com/sec/1Erm7HD" },
+  { img: "https://sancarock.com/propaganda/img/009.gif", link: "https://mercadolivre.com/sec/2hhCKB3" }
 ];
 
 const backgrounds = ["white.png", "grey.png", "fundored.png"];
