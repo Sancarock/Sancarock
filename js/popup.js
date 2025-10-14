235b438a3c0e4656347d6d6107dc84bedbf0857d
Sancarock <sancanight@gmail.com>
2025-10-13 21:45:26 -0300

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,6 @@
 function abrir_radio_popup() {
     window.open(
-        "original", 
+        "player", 
         "", 
         "width=550,height=850,toolbar=no,location=no,directories=no,status=yes,menubar=no,scrollbars=no,resizable=no"
     );
@@ -17,3 +17,4 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
 });
+
